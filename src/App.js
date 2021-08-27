c3272cc1d20c181edb598f771352e42bc1d2a20f
Maria Milagros Jaramillo Rios <mariwrios@gmail.com>
2021-08-26 21:42:30 -0400

    ahora onesignal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import logo from './logo.svg';
-import firebase from './firebase'
+
 import './App.css';
-import { useEffect } from 'react';
+
 
 function App() {
   
-  useEffect(() => {
-    const msg = firebase.messaging();
-    msg.getToken().then((currentToken) => {
-      if (currentToken) {
-        console.warn('token', currentToken)
-      } else {
-        console.log('No registration token available. Request permission to generate one.');
-      }
-    }).catch((err) => {
-      console.log('An error occurred while retrieving token. ', err);
-    });
-
-   }, [])
- 
-
 
   return (
     <div className="App">
