0779a5f5b0d1398eba56ab6989994959e69d95f7
Maria Milagros Jaramillo Rios <mariwrios@gmail.com>
2021-08-27 02:19:00 -0400

    onesignal codigo de prueba

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 
 import './App.css';
+import { useForm } from './hooks/useForm';
+import { useEffect, useState } from 'react';
+import { sendNotification } from './helper/sendNorification';
 
 
 function App() {
-  
+ const [message, setMessage] = useState()
+  const [formValues, handleInputChange, reset] = useForm({
+    msg:''
+  })
+  const { msg } = formValues;
+
+  useEffect(() => {
+  setMessage(msg) 
+   sendNotification(message)
+  }, [reset])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button>
+        <h1>Send a message! 💅</h1>
+        <input type='text' onChange={handleInputChange} name='msg' value={msg}/>
+        <button onClick={reset}>
         Click me!
       </button>
       </header>
---
+++ src/helper/sendNorification.js
@@ -0,0 +1,22 @@
+export const sendNotification = (body) => {
+ 
+  const key = 'ZTI5NWJlMDgtODA0OC00MWVmLWI4ZDAtMDRhMWU3Y2JjYjQx';
+ 
+  fetch('https://onesignal.com/api/v1/notifications', {
+    'method': 'POST',
+    'headers': {
+      'Authorization': 'Basic' + key,
+      'Content-Type': 'application/json'
+    },
+    'body': JSON.stringify({
+        "included_segments":["AdminSuscribe"],
+        "app_id": "d43d203b-53fe-4647-b210-757059b037e9",
+        "contents":{"en": `${body}`, "es": `${body}`},
+        "headings":{"en": "Notificacion Witi", "es": "Notificacion Witi"} 
+    })
+  }).then(function(response) {
+    console.log('Respuesta:',response);
+  }).catch(function(error) {
+    console.error(error);
+  })
+}
